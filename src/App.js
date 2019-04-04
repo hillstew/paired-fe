2c353c1d156db34261a90b4efee2718333804a4c
Hillary Stewart <hillarystewart4427@gmail.com>
2019-04-04 13:24:13 -0600

    Add routes to App and render Sidebar
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/main.scss';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Sidebar from './containers/Sidebar';
 
 class App extends Component {
   render() {
-    return <div className="App">App is running</div>;
+    return (
+      <div className="App">
+        <Sidebar />
+        <div>
+          <header>hi</header>
+          <Switch>
+            <Route path="/schedule" render={() => <div>path: /schedule</div>} />
+            <Route
+              path="/avail-pairings"
+              render={() => <div>path: /avail-pairings</div>}
+            />
+            <Route exact path="/" render={() => <div>path: /</div>} />
+            <Route render={() => <div>ERRORRRRRRRR</div>} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
-export default App;
+export default withRouter(App);
