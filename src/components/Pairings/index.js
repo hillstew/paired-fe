41b0d1924dba2010efa9c134445dd95a15fad739
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-06 17:02:16 -0600

    Add Pairings
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

---
+++ src/components/Pairings/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import StudentCard from '../StudentCard';
+
+export const createStudents = (openPairings) => {
+  return openPairings.reduce((acc, curr) => {
+    let found = false;
+    acc.forEach(student => {
+      if (student.id === curr.pairer.id) {
+        found = true;
+        student[curr.time] = curr.id;
+      }
+    });
+
+    if (!found) {
+      let student = {
+        ...curr.pairer,
+        morning: '',
+        lunch: '',
+        afternoon: ''
+      };
+      student[curr.time] = curr.id;
+      acc.push(student);
+    }
+
+    return acc;
+  }, []);
+};
+
+export const Pairings = ({ openPairings, history }) => {
+  const studentRows = createStudents(openPairings).map(student => {
+    return <StudentCard student={student} key={student.id} history={history}/>
+  });
+
+  return (
+  <div className='Pairings'>
+    {studentRows}
+  </div>
+  )
+};
