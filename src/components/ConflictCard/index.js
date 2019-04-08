85990f3c75799b9b94fb28ef28c21480658e9f9a
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-08 14:58:56 -0600

    Add classNames for buttons on ConflictCard

--- src/components/ConflictCard/index.js
+++ src/components/ConflictCard/index.js
@@ -9,10 +9,10 @@ export const ConflictCard = ({ date, time, history }) => {
       <p>
         Please go back and choose a different date/time or adjust your schedule
       </p>
-      <button onClick={() => history.push('/book-pairing')}>
+      <button className='ConflictCard--button' onClick={() => history.push('/book-pairing')}>
         Choose Another Pairing
       </button>
-      <button onClick={() => history.push('/schedule')}>View Schedule</button>
+      <button className='ConflictCard--button--schedule' onClick={() => history.push('/schedule')}>View Schedule</button>
     </div>
   );
 };
