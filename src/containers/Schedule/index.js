eb272bc2571f4d9c213c5d1f7742d21bf741b9a9
Hillary Stewart <hillarystewart4427@gmail.com>
2019-04-08 14:34:58 -0600

    Refactor CurrentOpenings to use ScheduleCard

--- src/containers/Schedule/index.js
+++ src/containers/Schedule/index.js
@@ -6,12 +6,15 @@ import { ScheduleCard } from '../../components/ScheduleCard';
 import { deletePairingThunk } from '../../thunks/deletePairingThunk';
 
 export class Schedule extends Component {
-  getOpenings = () => {
+  filterOpenings = () => {
     const { schedule } = this.props;
-    return schedule.filter(pairing => pairing.pairee === null);
+    const openings = schedule.filter(pairing => pairing.pairee === null);
+    return openings.map(booking => {
+      return <ScheduleCard booking={booking} person={null} key={booking.id} />;
+    });
   };
 
-  showPaireeBookings = () => {
+  filterPaireeBookings = () => {
     const { schedule, user } = this.props;
     const bookings = schedule.filter(
       pairing => pairing.pairee !== null && pairing.pairee.name === user.name
@@ -20,14 +23,14 @@ export class Schedule extends Component {
       return (
         <ScheduleCard
           booking={booking}
-          type="pairee"
-          cancelPairing={this.cancelPairing}
+          person={booking.pairer}
+          key={booking.id}
         />
       );
     });
   };
 
-  showPairerBookings = () => {
+  filterPairerBookings = () => {
     const { schedule, user } = this.props;
     const bookings = schedule.filter(
       pairing => pairing.pairee !== null && pairing.pairer.name === user.name
@@ -36,26 +39,36 @@ export class Schedule extends Component {
       return (
         <ScheduleCard
           booking={booking}
-          type="pairer"
-          cancelPairing={this.cancelPairing}
+          person={booking.pairee}
+          key={booking.id}
         />
       );
     });
   };
 
-  cancelPairing = id => {
-    const { deletePairingThunk } = this.props;
-    deletePairingThunk(id);
-  };
-
   render() {
     const { user } = this.props;
     return (
-      <div>
-        <CurrentOpenings openings={this.getOpenings()} user={user} />
-        <div>
-          {this.showPaireeBookings()}
-          {this.showPairerBookings()}
+      <div className="Schedule">
+        <h2 className="Schedule-h2">
+          {user.name}, here is your pairing schedule{' '}
+          <span role="img" aria-label="rocket ship emoji">
+            🚀
+          </span>
+        </h2>
+        <div className="ScheduleCards--div">
+          <div>
+            <h2>Giving help</h2>
+            {this.filterPairerBookings()}
+          </div>
+          <div>
+            <h2>Receiving help</h2>
+            {this.filterPaireeBookings()}
+          </div>
+          <div>
+            <h2>Open to pair</h2>
+            {this.filterOpenings()}
+          </div>
         </div>
       </div>
     );
