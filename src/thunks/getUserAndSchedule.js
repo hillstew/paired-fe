83a8ef6c84fed60aca3a701919b92b6fe1217bb2
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-08 15:40:48 -0600

    Refactor getUserAndSchedule thunk

--- src/thunks/getUserAndSchedule.js
+++ src/thunks/getUserAndSchedule.js
@@ -2,20 +2,19 @@ import { fetchData } from '../utils';
 import { setError, setLoading, setSchedule, setUser } from '../actions';
 import * as gql from '../queries';
 
-export const getUserAndSchedule = () => {
+export const getUserAndSchedule = (userID) => {
   return async dispatch => {
+    dispatch(setLoading(true));
     try {
-      dispatch(setLoading(true));
-      const userQuery = gql.getUser('Hillary');
+      const userQuery = gql.getUser(userID);
       const userRes = await fetchData(userQuery);
       const pairingQuery = gql.getUserPairings(userRes.getUser.id);
       const pairingRes = await fetchData(pairingQuery);
-      dispatch(setLoading(false));
       dispatch(setUser(userRes.getUser));
       dispatch(setSchedule(pairingRes.getUserPairings));
     } catch (error) {
-      dispatch(setLoading(false));
       dispatch(setError(error.message));
     }
+    dispatch(setLoading(false));
   };
 };
