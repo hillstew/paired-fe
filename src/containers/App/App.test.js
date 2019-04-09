2e10571ca25fc00a4c9f886f548a6bf404d0fed3
Hillary Stewart <hillarystewart4427@gmail.com>
2019-04-09 14:32:19 -0600

    Fix travis ci error in App test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -4,7 +4,7 @@ import { App } from './App';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { rootReducer } from '../../Reducers';
+import { rootReducer } from '../../reducers';
 import * as actions from '../../actions';
 import { signInUser } from '../../thunks/signInUser';
 import { BrowserRouter } from 'react-router-dom';
@@ -14,30 +14,30 @@ jest.mock('../../thunks/signInUser');
 
 beforeAll(() => {
   firebase.initializeApp({
-    apiKey: "AIzaSyBZztEJm_KxmHfMGaxYgLIFah2XAdS5mh0",
-    authDomain: "paired-turing.firebaseapp.com"
+    apiKey: 'AIzaSyBZztEJm_KxmHfMGaxYgLIFah2XAdS5mh0',
+    authDomain: 'paired-turing.firebaseapp.com'
   });
 });
 
 describe('App', () => {
-    const mockProps = {
-      hasError: '',
-      isLoading: false,
-      user: {},
-      getUser: jest.fn()
-    };
+  const mockProps = {
+    hasError: '',
+    isLoading: false,
+    user: {},
+    getUser: jest.fn()
+  };
 
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      const store = createStore(rootReducer);
-      ReactDOM.render(
-        <BrowserRouter>
-          <Provider store={store}>
-            <App {...mockProps} />
-          </Provider>
-        </BrowserRouter>,
-        div
-      );
-      ReactDOM.unmountComponentAtNode(div);
-    });
-})
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const store = createStore(rootReducer);
+    ReactDOM.render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <App {...mockProps} />
+        </Provider>
+      </BrowserRouter>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
