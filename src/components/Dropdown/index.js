19b6a748362f708e9bab0adc456d253c3a73ea3f
Hillary Stewart <hillarystewart4427@gmail.com>
2019-04-09 19:47:02 -0600

    Fix quotes to be singles
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-export const Dropdown = ({ options, label, handleOptionChange }) => {
+export const Dropdown = ({ options, label, handleChange }) => {
   return (
-    <div className="Dropdown--div">
-      <label htmlFor={label}> Choose a {label}</label>
+    <div className='Dropdown--div'>
+      <label htmlFor={label}>{label}</label>
       <select
-        className="Dropdown--select"
+        className='Dropdown--select'
         name={label}
-        onChange={event => handleOptionChange(event)}>
-        <option value="">Please choose an option</option>
+        onChange={event => handleChange(event)}>
+        <option value=''>Please choose an option</option>
         {options.map(optionItem => (
-          <option key={"option" + optionItem} value={optionItem}>
+          <option key={'option' + optionItem} value={optionItem}>
             {optionItem}
           </option>
         ))}
