61a97565e39d28bbe28ecd9dc5b75bf3c0b1509b
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-30 10:20:46 -0600

    Update Availability snapshots
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/containers/Availability/index.test.js
+++ src/containers/Availability/index.test.js
@@ -4,19 +4,40 @@ import { Availability } from './index';
 
 describe('Availability', () => {
   let wrapper;
-  let mockHandleClick = jest.fn();
-  let mockAvailabilities = [true, false, true, false, true, false];
+  const mockProps = {
+    handleClick: jest.fn(),
+    availabilities: [
+      true,
+      false,
+      true,
+      false,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true
+    ],
+    match: {
+      path: '/set-availability'
+    }
+  }
 
   beforeEach(() => {
-    wrapper = shallow(
-      <Availability
-        handleClick={mockHandleClick}
-        availabilities={mockAvailabilities}
-      />
-    );
+    wrapper = shallow(<Availability {...mockProps} />);
   });
 
-  it('should match the snapshot', () => {
+  it('should match the snapshot when the path is /set-availability', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when the path is /edit-availability', () => {
+    wrapper = shallow(<Availability {...mockProps} match={{ path: '/edit-availability' }} />);    
     expect(wrapper).toMatchSnapshot();
   });
 });
