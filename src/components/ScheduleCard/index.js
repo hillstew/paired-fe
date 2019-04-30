7d150e6dd636afe6d56142e26124960e650fbf51
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-30 13:49:12 -0600

    Update ScheduleCard with titles for each displayed item
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>
    Co-authored-by: Jeo D <e8gea6w5z8id@opayq.com>

--- src/components/ScheduleCard/index.js
+++ src/components/ScheduleCard/index.js
@@ -5,28 +5,27 @@ import PropTypes from 'prop-types';
 export const ScheduleCard = ({ booking, person, deletePairing }) => {
   const { notes, time, date } = booking;
   return (
-    <div className="ScheduleCard">
+    <div className='ScheduleCard'>
       {person && (
         <Fragment>
           <h3>{person.name}</h3>
-          <p>{person.pronouns}</p>
+          <p>Pronouns: {person.pronouns}</p>
+          <p>Slack: {person.slack}</p>
           <p>
-            {person.program} Mod {person.module}
+            Program: {person.program} Mod {person.module}
           </p>
-          <p>Notes: {notes ? notes : `${person.slack} on Slack`}</p>
+          <p>Notes: {notes ? notes : 'no notes'}</p>
         </Fragment>
       )}
-      <p>
+      <p className='ScheduleCard--date'>
         {date} at {determineDisplayTime(time)}
       </p>
       {!person && (
-        <button onClick={() => deletePairing(booking.id)}>
-          Cancel
-        </button>
+        <button onClick={() => deletePairing(booking.id)}>Cancel</button>
       )}
     </div>
   );
-};
+ };
 
 ScheduleCard.propTypes = {
   booking: PropTypes.object,
