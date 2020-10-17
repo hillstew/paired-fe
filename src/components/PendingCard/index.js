2d89b7f5bb081ca1c4d343680a58c16e75ae943c
Becky Robran <rerobran@gmail.com>
2020-10-17 12:32:46 -0600

    adds functionality to discontinue button

--- src/components/PendingCard/index.js
+++ src/components/PendingCard/index.js
@@ -2,20 +2,16 @@ import React, { useState } from 'react'
 import DenyReasons from '../../components/DenyReasons'
 
 const PendingCard = ({ pendingPebbles, userId, activateRockAndPebble, declineRockPebbleRelationship }) => {
-  const [ denyPebble, setdenyPebble ] = useState(false);
+  const [ denyPebble, setdenyPebble ] = useState('');
 
-  const createList = skills => {
-    return skills.map((skill, i) => {
-      return (
-        <li key={i} className='PebbleCard-li'>
-          {skill}
-        </li>
-      )
-    })
+  const handleSubmitDeny = (event) => {
+    setTimeout(() => {
+      setdenyPebble(event);
+    }, 100);
   }
 
-  const handleSubmitDeny = () => {
-    setdenyPebble(true);
+  const callback = (cancel) => {
+    setdenyPebble(cancel)
   }
 
   const handleSubmitAccept = async (pebbleId) => {
@@ -24,40 +20,26 @@ const PendingCard = ({ pendingPebbles, userId, activateRockAndPebble, declineRoc
 
   const pendingDisplay = pendingPebbles.map((pebble, i) => 
     <div key={i}>
-        {console.log(pebble)}
       <h2>{pebble.name} ({pebble.pronouns})</h2>
       <img src={pebble.image} alt={pebble.name} className='PebbleCard--image' />
       <p>{pebble.program} - Mod {pebble.module}</p>
       <p>Slack: {pebble.slack}</p>
-      <div className='PebbleCard--skills-div'>
-        <p className='PebbleCard--skills-p'>Skills</p>
-        <ul className='PebbleCard--ul'>{createList(pebble.skills)}</ul>
+      <div className='RockAndPebble--opt--div'>
+            { denyPebble !== pebble.id ? 
+          <>
+        <button className='PebbleCard--discon-btn' onClick={() => handleSubmitDeny(pebble.id)}>Say No To Relationship</button> <br></br>
+        <button className='PebbleCard--discon-btn' onClick={() => handleSubmitAccept(pebble.id)}>Say Yes To Relationship</button>
+        </> :
+         <DenyReasons  parentCallback={callback} userId = {userId} declineRockPebbleRelationship= {declineRockPebbleRelationship} pebbleId = {pebble.id}/>    
+        }
+            </div> 
       </div>
-
-      <div className='RockAndPebble--opt--div'>  
-      { !denyPebble &&
-        <div>
-           <button className='PebbleCard--discon-btn' onClick={() => handleSubmitDeny()}>Say No To Relationship</button> 
-           <p> </p>
-         <button className='PebbleCard--discon-btn' onClick={() => handleSubmitAccept(pebble.id)}>Say Yes To Relationship</button>
-         </div>
-      }
-        </div>
-
-        <div className='RockAndPebble--opt--div'>  
-        <div>
-            <DenyReasons userId = {userId} declineRockPebbleRelationship= {declineRockPebbleRelationship} pebbleId ={pebble.id}/>
-         </div>
-        </div> 
-
-    </div>
   )
-  return (
-      <section>
-          <h2 className='RockAndPebble--header--h2'>Pending Pebble(s)</h2>
-          <div className='RockAndPebble--opt--div'> 
+  return ( 
+    <section className='PebbleCard--section'>
+      <div className='PebbleCard--div'>
         {pendingDisplay}
-        </div>
+      </div>
     </section>
   )
 }
