d3e942a878838ef128f3c3a8fe77b428b05c3224
Becky Robran <rerobran@gmail.com>
2020-10-18 10:52:27 -0600

    adds userRelationship

--- src/thunks/discontinueRockPebble.js
+++ src/thunks/discontinueRockPebble.js
@@ -2,11 +2,11 @@ import { fetchData} from '../utils';
 import { setError, setLoading, setRockAndPebble } from '../actions';
 import * as gql from '../queries';
 
-export const discontinueRockPebbleRelationship = (rockId, pebbleId, reason) => {
+export const discontinueRockPebbleRelationship = (rockId, pebbleId, reason, userRelationship) => {
   return async dispatch => {
     try {
       dispatch(setLoading(true));
-      const query = gql.discontinueRockPebbleRelationship(rockId, pebbleId, reason);
+      const query = gql.discontinueRockPebbleRelationship(rockId, pebbleId, reason, userRelationship);
       const result = await fetchData(query);
       dispatch(setLoading(false));
       dispatch(setRockAndPebble(result.discontinueRockPebbleRelationship));
